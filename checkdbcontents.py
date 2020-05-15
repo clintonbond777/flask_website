1fc846084e7a9f8775990b2f9ca6e3145505bc07
clintonbond777 <clintonbond777@gmail.com>
2020-05-15 19:25:38 -0400

    updated database schema

--- checkdbcontents.py
+++ checkdbcontents.py
@@ -1,18 +1,73 @@
 from flask_website import db
-from flask_website.models import User, Program
+from flask_website.models import User, Program, Geometry, Case
 
+### Temporary load in data: drop when connected to flask app
+db.drop_all()
+db.create_all()
+
+u1 = User(username = 'Jeff', email = 'jeff@123.com', password = '123')
+u2 = User(username = 'Jeff1', email = 'jeff1@123.com', password = '1234')
+
+db.session.add(u1)
+db.session.add(u2)
+db.session.commit()
+
+print(u1.id)
+
+p1 =Program(name = 'WRC', 
+    description = 'WRC CAR', 
+    created_by = u1.id) 
+
+p2 =Program(name = 'NIGHTHAWK', 
+    description = 'NASCAR', 
+    created_by = u2.id)
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.commit()
+
+g1 = Geometry(name = 'Baseline-1-04', 
+    description = '2022 baseline car', 
+    created_by = u2.id)
+
+g2 = Geometry(name = 'Baseline-1-04-WA-FF-01', 
+    description = 'Front Splitter Extension (-x)', 
+    created_by = u1.id)
+
+db.session.add(g1)
+db.session.add(g2)
+db.session.commit() 
+
+c1 =Case(name = 'baseline run', 
+    description = '2022 baseline preliminary run', 
+    created_by = u1.id, 
+    project_id = p1.id,  
+    geometry_id = g2.id
+    )
+
+c2 =Case(name = 'WA-FF-01 Part Swap ', 
+    description = '2022 part swap', 
+    created_by = u1.id, 
+    project_id = p1.id, 
+    geometry_id = g2.id 
+    )
+
+db.session.add(c1)
+db.session.add(c2)
+db.session.commit()
 
 for u in User.query.all():
-    print(u)
-
-
-print('\n')
-
+    print(u.username)
+    
 for p in Program.query.all():
-    print(p)
+    print(p.name)
 
-user = User.query.filter_by(id = '3').first()
-print(user.program)
+for g in Geometry.query.all():
+    print(g.name)
 
+for c in Case.query.all():
+    print(c.name)
 
-print(user)
+user = User.query.filter_by(id = '2').first()
+print(p1.created_by)
+
--- flask_website/models.py
+++ flask_website/models.py
@@ -7,36 +7,47 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model,UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20),unique=True, nullable=False)
     email = db.Column(db.String(120),unique=True, nullable=False)
     image_file = db.Column(db.String(20), default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     ### Links to Progam db model 
-    program = db.relationship('Program', backref='creator', lazy=True)
+    create_program= db.relationship('Program', backref='creator', lazy=True)
 
     def __repr__(self):
-        return f"User ('{self.id}','{self.username}'),'{self.email}','{self.image_file}','{self.password}'"
+        return
+        f"User|'{self.id}','{self.username}','{self.email}','{self.image_file}','{self.password}','{self.create_program}'"
 
 class Program(db.Model):
+    __tablename__ = 'program'  
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),unique=True, nullable=False)
     description = db.Column(db.Text,unique=False, nullable=False)
     created_date = db.Column(db.DateTime,nullable=False, default = datetime.utcnow)
     created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
+    
+    ### Relationship
+    project = db.relationship("Project", back_populates = "program") # forward
 
     def __repr__(self):
-        return f"Program ('{self.name}'),'{self.description}','{self.created_date}', '{self.created_by}'"
-
-class Project(db.model):
+        return f"Program|'{self.name}'),'{self.description}','{self.created_date}', '{self.created_by}'" 
+        
+class Project(db.Model):
+    __tablename__ = 'project'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),unique=True, nullable=False)
     description = db.Column(db.Text,unique=False, nullable=False)
     created_date = db.Column(db.DateTime,nullable=False, default = datetime.utcnow)
-    created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
-    program = realtionship("Program")
-
+    created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False) 
+    program_id = db.Column(db.Integer,db.ForeignKey('program.id'), nullable = False)
+    program = db.relationship("Program", back_populates = "project")
+    
+    def __repr__(self):
+        return f"Program|'{self.name}'),'{self.description}','{self.created_date}','{self.created_by}', '{self.program_id}','{self.program}'"
 class Geometry(db.Model):
+    __tablename__ = 'geometry'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),unique=True, nullable=False)
     description = db.Column(db.Text,unique=False, nullable=False)
@@ -44,27 +55,33 @@ class Geometry(db.Model):
     created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
     
     def __repr__(self):
-        return f"Geometry ('{self.name}'),'{self.description}','{self.created_date}'"
+        return f"Geometry|'{self.name}'),'{self.description}','{self.created_date}'"
 
 class Part(db.Model):
+    __tablename__ = 'part'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),unique=True, nullable=False)
     description = db.Column(db.Text,unique=False, nullable=False)
     created_date = db.Column(db.DateTime,nullable=False, default = datetime.utcnow)
     created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
-    geometry =
     
-class Case(db.model):
+class Case(db.Model):
+    __tablename__ = 'case'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20),unique=True, nullable=False)
     description = db.Column(db.Text,unique=False, nullable=False)
     created_date = db.Column(db.DateTime,nullable=False, default = datetime.utcnow)
     created_by = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
-    geometry = relationship("Geometry")
-    
+    project_id= db.Column(db.Integer, db.ForeignKey('project.id'), nullable = False)
+    geometry_id = db.Column(db.Integer, db.ForeignKey('geometry.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Case|('{self.id}','{self.name}'),'{self.description}','{self.project_id}','{self.geometry_id}'"
+
 class RideHeight(db.Model):
-     id = db.Column(db.Integer, primary_key=True)
-    FRH = db.Column(db.Float(), nullable=False)
-    RRH = db.Column(db.Float(), nullable=False)
-    YAW = db.Column(db.Float(), nullable=False)
+    __tablename__ = 'rideheight'  
+    id = db.Column(db.Integer, primary_key=True)
+    frh= db.Column(db.Float(), nullable=False)
+    rrh= db.Column(db.Float(), nullable=False)
+    yaw= db.Column(db.Float(), nullable=False)
 
